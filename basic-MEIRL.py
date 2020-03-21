a1267cf09cd2e94ca64f3b18e055e03edaf36c4c
digiovannia <51431932+digiovannia@users.noreply.github.com>
2020-03-20 20:04:37 -0400

    fixed huge very dumb bug in evaluate functions

--- basic-MEIRL.py
+++ basic-MEIRL.py
@@ -824,7 +824,7 @@ def evaluate_vs_uniform(theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
         
         theta_star = MEIRL_unif(theta, beta, traj_data, TP, state_space,
          action_space, B, m, M, Ti, learn_rate, 5, y_t_nest_unif, SGD_unif, centers_x, centers_y)[0]
-        reward_est = lin_rew_func(np.random.normal(size=d), state_space,
+        reward_est = lin_rew_func(theta_star, state_space,
                                   centers_x, centers_y)
         est_policy = Qlearn(0.5, 0.8, 0.1, Q_ITERS, 20, state_space,
           action_space, reward_est, init_policy, init_Q)[0]
@@ -832,6 +832,38 @@ def evaluate_vs_uniform(theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
         plt.plot(np.cumsum(est_rew), color='g')
         unif_total.append(np.sum(est_rew))
     return true_total, AEVB_total, unif_total
+
+def evaluate_det_vs_unif(lam, theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
+                        state_space, action_space, rewards, init_policy,
+                        init_Q, J, B, m, M, Ti, learn_rate, traj_data,
+                        centers_x, centers_y):
+    true_rew = cumulative_reward(reps, policy, T, state_space, rewards)
+    plt.plot(np.cumsum(true_rew), color='b') 
+    true_total = np.sum(true_rew)
+    det_total = []
+    unif_total = []
+    for _ in range(J):
+        theta_star = MEIRL_det_reg(lam, theta, alpha, traj_data, TP, state_space,
+         action_space, B, m, M, Ti, learn_rate, 5, y_t_nest_unif, SGD_unif,
+         centers_x, centers_y, plot=False)[0]
+        reward_est = lin_rew_func(theta_star, state_space,
+                                  centers_x, centers_y)
+        est_policy = Qlearn(0.5, 0.8, 0.1, Q_ITERS, 20, state_space,
+          action_space, reward_est, init_policy, init_Q)[0]
+        est_rew = cumulative_reward(reps, est_policy, T, state_space, rewards)
+        plt.plot(np.cumsum(est_rew), color='g')
+        det_total.append(np.sum(est_rew))
+
+        theta_star = MEIRL_unif(theta, beta, traj_data, TP, state_space,
+         action_space, B, m, M, Ti, learn_rate, 5, y_t_nest_unif, SGD_unif, centers_x, centers_y)[0]
+        reward_est = lin_rew_func(theta_star, state_space,
+                                  centers_x, centers_y)
+        est_policy = Qlearn(0.5, 0.8, 0.1, Q_ITERS, 20, state_space,
+          action_space, reward_est, init_policy, init_Q)[0]
+        est_rew = cumulative_reward(reps, est_policy, T, state_space, rewards)
+        plt.plot(np.cumsum(est_rew), color='g')
+        unif_total.append(np.sum(est_rew))
+    return true_total, det_total, unif_total
 
 def evaluate_vs_det(theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
                         state_space, action_space, rewards, init_policy,
@@ -855,7 +887,7 @@ def evaluate_vs_det(theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
         theta_star = MEIRL_det(theta, alpha, traj_data, TP, state_space,
          action_space, B, m, M, Ti, learn_rate, 5, y_t_nest_unif, SGD_unif,
          centers_x, centers_y, plot=False)[0]
-        reward_est = lin_rew_func(np.random.normal(size=d), state_space,
+        reward_est = lin_rew_func(theta_star, state_space,
                                   centers_x, centers_y)
         est_policy = Qlearn(0.5, 0.8, 0.1, Q_ITERS, 20, state_space,
           action_space, reward_est, init_policy, init_Q)[0]
@@ -890,7 +922,7 @@ def evaluate_vs_uniform_init(theta, alpha, sigsq, phi, beta, TP, reps, policy, T
         
         theta_star = MEIRL_unif(theta, beta, traj_data, TP, state_space,
          action_space, B, m, M, Ti, learn_rate, 5, y_t_nest_unif, SGD_unif, centers_x, centers_y)[0]
-        reward_est = lin_rew_func(np.random.normal(size=d), state_space,
+        reward_est = lin_rew_func(theta_star, state_space,
                                   centers_x, centers_y)
         est_policy = Qlearn(0.5, 0.8, 0.1, Q_ITERS, 20, state_space,
           action_space, reward_est, init_policy, init_Q)[0]
@@ -982,7 +1014,11 @@ def alpha_grad_det(glogZ_alpha, R_all, E_all):
 def theta_grad_det(data, beta, state_space, glogZ_theta, centers_x, centers_y):
     gradR = grad_lin_rew(data, state_space, centers_x, centers_y) # m x d x Ti 
     return -glogZ_theta + np.einsum('ij,ikj->k', beta, gradR)
-   #(beta[:,None,:]*gradR).sum(axis=2).sum(axis=0) # each term is quite large
+
+def theta_grad_det_reg(lam, theta, data, beta, state_space, glogZ_theta, centers_x, centers_y):
+    gradR = grad_lin_rew(data, state_space, centers_x, centers_y) # m x d x Ti 
+    penalty = lam*theta
+    return -glogZ_theta + np.einsum('ij,ikj->k', beta, gradR) - penalty
 
 def SGD_unif(theta, beta, g_theta, g_beta, learn_rate):
     theta = theta + learn_rate*g_theta
@@ -1041,6 +1077,73 @@ def MEIRL_det(theta, alpha, traj_data, TP, state_space,
             lik.append(loglikelihood)
               
             g_theta = theta_grad_det(data, beta, state_space, glogZ_theta, centers_x, centers_y)
+            g_alpha = alpha_grad_det(glogZ_alpha, R_all, E_all)
+          
+            theta_m, alpha_m, = theta, alpha
+            theta = y_theta + learn_rate*g_theta
+            alpha = y_alpha + learn_rate*g_alpha
+            
+            mult = (tm - 1)/t
+            y_theta = theta + mult*(theta - theta_m)
+            y_alpha = alpha + mult*(alpha - alpha_m)
+            
+            learn_rate *= 0.99
+            tm = t
+            
+            if loglikelihood > best:
+                best = loglikelihood
+                best_theta = y_theta.copy()
+                best_alpha = y_alpha.copy()
+            elif loglikelihood < last_lik:
+                y_theta = theta.copy()
+                y_alpha = alpha.copy()
+                tm = 1
+                
+            last_lik = loglikelihood
+    if plot:
+        plt.plot(lik)
+    return best_theta, best_alpha
+
+def MEIRL_det_reg(lam, theta, alpha, traj_data, TP, state_space,
+         action_space, B, m, M, Ti, learn_rate, reps, y_t, update, centers_x,
+         centers_y, plot=True):
+    '''
+    Need the expert trajectories
+
+    y_t is the function used to define modified iterate for Nesterov, if
+    applicable
+    
+    update is e.g. SGD or Adam
+    '''
+    impa = uniform_action_sample(action_space, M)
+    N = len(traj_data)
+    lik = []
+    theta_m = np.zeros_like(theta)
+    alpha_m = np.zeros_like(alpha)
+    y_theta = theta.copy()
+    y_alpha = alpha.copy()
+    best = -np.inf
+    best_theta = theta_m.copy()
+    best_alpha = alpha_m.copy()
+    tm = 1
+    last_lik = -np.inf
+    # while error > eps:
+    for _ in range(reps):
+        permut = list(np.random.permutation(range(N)))
+        for n in permut:
+            t = 1/2*(1 + np.sqrt(1 + 4*tm**2))
+            
+            data = np.array(traj_data[n]) # m x 2 x Ti
+            R_all, E_all = RE_all(y_theta, data, TP, state_space, m, centers_x, centers_y)
+            beta = np.einsum('ij,ijk->ik', y_alpha, E_all)
+            
+            logZvec, glogZ_theta, glogZ_alpha = logZ_det(beta,
+              impa, y_theta, data, M, TP, R_all, E_all, action_space, centers_x, centers_y)
+          
+            loglikelihood = loglik(state_space, Ti, beta, data, TP, m, R_all, logZvec).sum()
+            lik.append(loglikelihood)
+              
+            g_theta = theta_grad_det_reg(lam, y_theta, data, beta, state_space, glogZ_theta, centers_x, centers_y)
             g_alpha = alpha_grad_det(glogZ_alpha, R_all, E_all)
           
             theta_m, alpha_m, = theta, alpha
