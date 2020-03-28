fc45500d2877ef5763e9dc15a45d567e7297d351
Anthony DiGiovanni <adigi@adigi-mbp.attlocal.net>
2020-03-28 15:27:26 -0400

    above

--- extra_funcs.py
+++ extra_funcs.py
@@ -438,6 +438,109 @@ def evaluate_vs_det(theta, alpha, sigsq, phi, beta, TP, reps, policy, T,
         print('.')
     return true_total, AEVB_total, det_total
 
+def ann_AEVB(theta, alpha, sigsq, phi, beta, traj_data, TP, state_space,
+         action_space, B, m, M, Ti, N, learn_rate, reps, centers_x, centers_y,
+         plot=True):
+    '''
+    Autoencoder with simulated annealing and Nesterov
+    '''
+    impa = list(np.random.choice(action_space, M))
+    elbos = []
+    phi_m = np.zeros_like(phi)
+    theta_m = np.zeros_like(theta)
+    alpha_m = np.zeros_like(alpha)
+    sigsq_m = np.zeros_like(sigsq)
+    y_phi = phi.copy()
+    y_theta = theta.copy()
+    y_alpha = alpha.copy()
+    y_sigsq = sigsq.copy()
+    best = -np.inf
+    best_phi = phi_m.copy()
+    best_theta = theta_m.copy()
+    best_alpha = alpha_m.copy()
+    best_sigsq = sigsq_m.copy()
+    time_since_best = 0
+    tm = 1
+    start_lr = learn_rate
+    normals = np.random.multivariate_normal(np.zeros(Ti), np.eye(Ti), (m, B))
+    
+    for _ in range(reps):
+        permut = list(np.random.permutation(range(N)))
+        for n in permut:
+            time_since_best += 1
+            t = 1/2*(1 + np.sqrt(1 + 4*tm**2))
+
+            data = np.array(traj_data[n])
+            reward_est = lin_rew_func(y_theta, state_space, centers_x,
+              centers_y)
+            R_all, E_all = RE_all(reward_est, data, TP, state_space, m,
+              centers_x, centers_y)
+            meanvec, denom, gvec, gnorm = grad_terms(normals, y_phi, y_alpha,
+              y_sigsq, y_theta, data, R_all, E_all, Ti, m)
+            logZvec, gZ_theta, gZ_alpha, gZ_sigsq, gZ_phi = logZ(y_sigsq,
+              normals, meanvec, denom, impa, reward_est, data, M, TP, R_all,
+              E_all, action_space, centers_x, centers_y, state_space, m, Ti)
+          
+            logprobdiff = elbo(state_space, Ti, y_sigsq, gnorm, data, TP, m,
+              normals, R_all, logZvec, meanvec, denom)
+            elbos.append(logprobdiff)
+            
+            if logprobdiff > best:
+                best = logprobdiff
+                best_phi = y_phi.copy()
+                best_theta = y_theta.copy()
+                best_alpha = y_alpha.copy()
+                best_sigsq = y_sigsq.copy()
+                time_since_best = 0
+              
+            g_phi = phi_grad_ae(y_phi, m, Ti, normals, denom, y_sigsq, gZ_phi)
+            g_theta = theta_grad_ae(gZ_theta, data, state_space, R_all, E_all,
+              y_sigsq, y_alpha, centers_x, centers_y)
+            g_alpha = alpha_grad_ae(gZ_alpha, E_all, R_all)
+            g_sigsq = sigsq_grad_ae(gZ_sigsq, normals, Ti, y_sigsq, gnorm,
+              denom, R_all, gvec)
+            
+            g_phi = g_phi / np.linalg.norm(g_phi)
+            g_theta = g_theta / np.linalg.norm(g_theta)
+            g_alpha = g_alpha / np.linalg.norm(g_alpha, 'f')
+            g_sigsq = g_sigsq / np.linalg.norm(g_sigsq)
+          
+            phi_m, theta_m, alpha_m, sigsq_m = phi, theta, alpha, sigsq
+            phi, theta, alpha, sigsq = GD(y_phi, y_theta, y_alpha, y_sigsq,
+              g_phi, g_theta, g_alpha, g_sigsq, learn_rate)
+            
+            mult = (tm - 1)/t
+            y_phi = phi + mult*(phi - phi_m)
+            y_phi[:,1] = np.maximum(y_phi[:,1], 0.01)
+            y_theta = theta + mult*(theta - theta_m)
+            #y_alpha = alpha + mult*(alpha - alpha_m)
+            y_alpha = np.maximum(alpha + mult*(alpha - alpha_m), 0)
+            y_sigsq = np.maximum(sigsq + mult*(sigsq - sigsq_m), 0.01)
+            
+            learn_rate *= 0.99
+            t += 1
+            
+            if time_since_best > RESET:
+                phi_m = np.zeros_like(phi)
+                theta_m = np.zeros_like(theta)
+                alpha_m = np.zeros_like(alpha)
+                sigsq_m = np.zeros_like(sigsq)
+                learn_rate = start_lr
+                phi += np.random.normal(scale=2*np.max(np.abs(phi)),
+                  size=phi.shape)
+                phi[:,1] = np.maximum(phi[:,1], 0.01)
+                theta += np.random.normal(scale=2*np.max(np.abs(theta)),
+                  size=theta.shape)
+                alpha += np.random.normal(scale=2*np.max(np.abs(alpha)),
+                  size=alpha.shape)
+                sigsq += np.random.normal(scale=2*np.max(np.abs(sigsq)),
+                  size=sigsq.shape)
+                sigsq = np.maximum(sigsq, 0.01)
+                time_since_best = 0
+    if plot:
+        plt.plot(elbos)
+    return best_theta, best_phi, best_alpha, best_sigsq
+
 def arr_radial(s, c, coef):
     '''
     Radial basis functions for the reward function, applied to an array of
